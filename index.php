2ca8d848fb1f0eced7e6fd80c834d3d780009f5a
Matt Cole <coledawg@gmail.com>
2015-09-22 19:22:54 -0400

    Initial file setup for P2

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+
+	<title>Matthew Cole P2 - XKCD Password Generator</title>
+	<meta charset='utf-8'>
+
+  <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+
+  <style>
+
+    h1
+    {
+      text-align: center;
+    }
+
+    </style>
+
+    <?php require "pwgenerator.php"; ?>
+
+</head>
+<body>
+
+</body>
