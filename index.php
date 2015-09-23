4b29325b074cd2476d61b3eea3f62638dd0652c8
Matt Cole <coledawg@gmail.com>
2015-09-22 20:17:19 -0400

    Updated index and started coding array

--- index.php
+++ index.php
@@ -9,16 +9,48 @@
 
   <style>
 
-    h1
+    img
     {
-      text-align: center;
+      display: block;
+      margin: auto;
+      width: 60%;
     }
 
+    .pwdisplay
+    {
+        text-align: center;
+        color: #009900;
+        background-color: #D2D2D2;
+        display: inline-block;
+    }
+
+    .wrapper
+    {
+			text-align: center;
+		}
     </style>
 
     <?php require "pwgenerator.php"; ?>
 
 </head>
 <body>
+
+<div class="wrapper">
+
+<h1>XKCD-Style Password Generator
+
+<form>
+
+
+</form>
+
+<p class="pwdisplay">
+ test - display - for - password
+</p>
+
+<a href= "http://xkcd.com/936/"><img src= "password_strength.png"></a>
+
+</div>
+
 
 </body>
---
+++ pwgenerator.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+
+$numWords = 4;    //default number of words to create a password with
+$useNum = FALSE;  //whether to add a number to the end of the password or not
+$useSym = FALSE;  //whether to add a symbol to the end of the password or not
+
+
+
+ ?>
